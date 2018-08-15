2aaea4c7f743064a9748a85a3b3627d220188d39
ewen_ <ewenhenderson@gmail.com>
2018-08-15 21:50:58 +0100

    go back to R...

---
+++ src/00-scrape.R
@@ -0,0 +1,36 @@
+
+# setup -------------------------------------------------------------------
+
+library(rvest)
+library(dplyr)
+library(stringr)
+library(purrr)
+
+# set parameters
+league_name = "premier-league"
+league_id = "GB1"
+season_id = 2018
+
+# set transfers url
+transfers_url <- paste0('https://www.transfermarkt.co.uk/', league_name,
+                        '/transfers/wettbewerb/', league_id, '/saison_id/', season_id)
+
+# read page
+transfers_html <- read_html(transfers_url)
+
+
+# scrape ---------------------------------------------------
+
+# get leagues club names
+clubs <- transfers_html %>% 
+  html_nodes(".table-header") %>%
+  html_text() %>% 
+  .[2:length(.)]
+
+# get leagues transfers
+transfers <- transfers_html %>% 
+  html_table(".responsive-table", header=TRUE, fill=TRUE)
+
+# return elements with expected transfer table form
+cond <- sapply(transfers, function(x) length(x) == 9)
+transfers <- transfers[cond]
