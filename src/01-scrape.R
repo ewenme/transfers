1dfe80cc9d01d10b201c1d835cb71fecdadf9b89
ewen_ <ewenhenderson@gmail.com>
2018-08-25 21:52:50 +0100

    set up scrape for other euro leagues.

--- src/01-scrape.R
+++ src/01-scrape.R
@@ -19,13 +19,29 @@ library(janitor)
 epl_transfers <- map_dfr(2000:2018, scrape_season_transfers, 
                          league_name = "premier-league", league_id = "GB1")
 
+# historic la liga transfers
 laliga_transfers <- map_dfr(2000:2018, scrape_season_transfers, 
                          league_name = "primera-division", league_id = "ES1")
+
+# historic bundes transfers
+bundes_transfers <- map_dfr(2000:2018, scrape_season_transfers, 
+                            league_name = "1-bundesliga", league_id = "L1")
+
+# historic serie a transfers
+seriea_transfers <- map_dfr(2000:2018, scrape_season_transfers, 
+                            league_name = "serie-a", league_id = "IT1")
+
+# historic ligue 1 transfers
+ligue1_transfers <- map_dfr(2000:2018, scrape_season_transfers, 
+                            league_name = "ligue-1", league_id = "FR1")
 
 
 # export ------------------------------------------------------------------
 
 write_csv(epl_transfers, path = file.path(".", "data", "premier-league-transfers.csv"))
 write_csv(laliga_transfers, path = file.path(".", "data", "primera-division-transfers.csv"))
+write_csv(bundes_transfers, path = file.path(".", "data", "bundesliga-transfers.csv"))
+write_csv(seriea_transfers, path = file.path(".", "data", "serie-a-transfers.csv"))
+write_csv(ligue1_transfers, path = file.path(".", "data", "ligue-1-transfers.csv"))
 
-rm(epl_transfers, laliga_transfers)
+rm(epl_transfers, laliga_transfers, bundes_transfers, seriea_transfers, ligue1_transfers)
